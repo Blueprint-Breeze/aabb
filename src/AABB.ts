5fbba63a5884f540ee2b2f4ae8b0fc8159dc95cd
yingpengsha <yingpengsha@kuaishou.com>
2024-01-08 19:43:36 +0800

    fix: type declarations

--- src/AABB.ts
+++ src/AABB.ts
@@ -11,36 +11,27 @@ export type Point<Is3D extends boolean = false> = Is3D extends true
   ? Point3D
   : Point2D
 
-export default class AABB<Is3D extends boolean = false> {
-  min: Point<Is3D>
-  max: Point<Is3D>
+class AABB<P extends Point<boolean> = Point2D> {
+  min: P
+  max: P
 
-  constructor (min: Point<true>, max: Point<true>)
-  constructor (min: Point<false>, max: Point<false>)
-  constructor (min: Point<Is3D>, max: Point<Is3D>) {
+  constructor (min: Point2D, max: Point2D)
+  constructor (min: Point3D, max: Point3D)
+  constructor (min: P, max: P) {
     this.min = min
     this.max = max
   }
 
   // factory
-  static from<Is3D extends true>(
-    min: Point<Is3D>,
-    max: Point<Is3D>
-  ): AABB<Is3D>
-  static from<Is3D extends false>(
-    min: Point<Is3D>,
-    max: Point<Is3D>
-  ): AABB<Is3D>
-  static from<Is3D extends boolean = false>(
-    min: Point<Is3D>,
-    max: Point<Is3D>
-  ): AABB<Is3D> {
-    return new AABB<Is3D>(min, max)
+  static from (min: Point2D, max: Point2D): AABB<Point2D>
+  static from (min: Point3D, max: Point3D): AABB<Point3D>
+  static from<P extends Point<boolean> = Point2D>(min: P, max: P): AABB<P> {
+    return new AABB<P>(min, max)
   }
 
   // other
-  clone (): AABB<Is3D> {
-    return new AABB<Is3D>(this.min, this.max)
+  clone (): AABB<P> {
+    return new AABB<P>(this.min, this.max)
   }
 
   toString (): string {
@@ -48,16 +39,19 @@ export default class AABB<Is3D extends boolean = false> {
   }
 }
 
-export class AABB2D extends AABB<false> {
-  // factory
-  static from (min: Point2D, max: Point2D): AABB2D {
-    return super.from(min, max)
-  }
-}
+// workaround from https://github.com/microsoft/TypeScript/issues/35387#issuecomment-559152671
+// subscribe feature to https://github.com/microsoft/TypeScript/issues/40451#issue-696949269
+export default AABB as {
+  new (min: Point2D, max: Point2D): AABB<Point2D>
+  new (min: Point3D, max: Point3D): AABB<Point3D>
+} & Omit<typeof AABB, 'new'>
 
-export class AABB3D extends AABB<true> {
-  // factory
-  static from (min: Point3D, max: Point3D): AABB3D {
-    return super.from(min, max)
-  }
-}
+export const AABB2D = AABB as {
+  new (min: Point2D, max: Point2D): AABB<Point2D>
+  from: (min: Point2D, max: Point2D) => AABB<Point2D>
+} & Omit<typeof AABB, 'new' | 'from'>
+
+export const AABB3D = AABB as {
+  new (min: Point3D, max: Point3D): AABB<Point3D>
+  from: (min: Point3D, max: Point3D) => AABB<Point3D>
+} & Omit<typeof AABB, 'new' | 'from'>
