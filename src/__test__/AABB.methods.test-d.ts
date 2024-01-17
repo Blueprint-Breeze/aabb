01f2f4f12b0894d48ac7b77be420c8b9ffe8da35
yingpengsha <yingpengsha@kuaishou.com>
2024-01-17 11:38:25 +0800

    fix: regroup test case

--- src/__test__/AABB.methods.test-d.ts
+++ src/__test__/AABB.methods.test-d.ts
@@ -1,7 +1,5 @@
 import { describe, expectTypeOf, test } from 'vitest'
-import {
-  AABB
-} from '..'
+import { AABB } from '..'
 
 describe('AABB methods test', () => {
   describe('AABB.is2D', () => {
@@ -9,20 +7,21 @@ describe('AABB methods test', () => {
       const instance = new AABB({ x: 1, y: 1 }, { x: 2, y: 2 })
       expectTypeOf(instance.is2D).toEqualTypeOf<true>()
     })
-
     test('returns false when 3D AABB', () => {
-      const instance = new AABB({ x: 1, y: 1 }, { x: 2, y: 2 })
-      expectTypeOf(instance.is3D).toEqualTypeOf<false>()
+      const instance = new AABB({ x: 1, y: 1, z: 1 }, { x: 2, y: 2, z: 2 })
+      expectTypeOf(instance.is2D).toEqualTypeOf<false>()
     })
+  })
 
-    test('returns false when 3D AABB', () => {
+  describe('AABB.is3D', () => {
+    test('returns true when 3D AABB', () => {
       const instance = new AABB({ x: 1, y: 1, z: 1 }, { x: 2, y: 2, z: 2 })
       expectTypeOf(instance.is3D).toEqualTypeOf<true>()
     })
 
-    test('returns false when 3D AABB', () => {
-      const instance = new AABB({ x: 1, y: 1, z: 1 }, { x: 2, y: 2, z: 2 })
-      expectTypeOf(instance.is2D).toEqualTypeOf<false>()
+    test('returns false when 2D AABB', () => {
+      const instance = new AABB({ x: 1, y: 1 }, { x: 2, y: 2 })
+      expectTypeOf(instance.is3D).toEqualTypeOf<false>()
     })
   })
 })
