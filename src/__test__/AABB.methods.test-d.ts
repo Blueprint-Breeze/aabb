3b34f6e59fb9321c0ee58e2cd0534e92ed286854
yingpengsha <yingpengsha@kuaishou.com>
2024-01-17 10:56:35 +0800

    refactor: split test cases

---
+++ src/__test__/AABB.methods.test-d.ts
@@ -0,0 +1,28 @@
+import { describe, expectTypeOf, test } from 'vitest'
+import {
+  AABB
+} from '..'
+
+describe('AABB methods test', () => {
+  describe('AABB.is2D', () => {
+    test('returns true when 2D AABB', () => {
+      const instance = new AABB({ x: 1, y: 1 }, { x: 2, y: 2 })
+      expectTypeOf(instance.is2D).toEqualTypeOf<true>()
+    })
+
+    test('returns false when 3D AABB', () => {
+      const instance = new AABB({ x: 1, y: 1 }, { x: 2, y: 2 })
+      expectTypeOf(instance.is3D).toEqualTypeOf<false>()
+    })
+
+    test('returns false when 3D AABB', () => {
+      const instance = new AABB({ x: 1, y: 1, z: 1 }, { x: 2, y: 2, z: 2 })
+      expectTypeOf(instance.is3D).toEqualTypeOf<true>()
+    })
+
+    test('returns false when 3D AABB', () => {
+      const instance = new AABB({ x: 1, y: 1, z: 1 }, { x: 2, y: 2, z: 2 })
+      expectTypeOf(instance.is2D).toEqualTypeOf<false>()
+    })
+  })
+})
